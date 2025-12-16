8d4a31112d140bacf1e14cb26d27aa405fce1554
Akoun Denzel KOUAME <ad.kouame@quarta.fr>
2025-12-16 11:14:33 +0100

    ssh conf

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.json({ message: "CI/CD Docker + Node.js fonctionne ! 🚀" });
+    res.json({ message: "CI/CD Docker + +ssh server+ Node.js fonctionne ! 🚀" });
 });
 
 app.listen(3000, () => {
